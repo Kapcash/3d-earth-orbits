0cfc94a22f9ebd0f2bdc109fd4b56ef51c4c730f
Florent Catiau-Tristant <florent.ct@orange.fr>
2021-11-06 13:49:23 +0100

    feat: rotation orbot working

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <Renderer ref="renderer" antialias :orbit-ctrl="{ enableDamping: true }" resize="window">
-    <Camera :position="{ z: 5 }" />
+    <Camera :position="{ z: 7, y: 3, x: 10 }" />
     <Scene ref="scene">
       <HemisphereLight cast-shadow color="#ffffbb" :intensity="0.5" />
       <PointLight cast-shadow :position="{ y: 50, z: 50 }" :intensity="1" />
@@ -19,11 +19,14 @@
 import { ref, Ref, onMounted } from 'vue'
 import { Box, Camera, Mesh, BasicMaterial, LambertMaterial, SphereGeometry, DirectionalLight, HemisphereLight, GltfModel, MeshPublicInterface, PointLight, Renderer, RendererPublicInterface, Scene } from 'troisjs'
 import * as THREE from 'three'
+import { useHelperGrid, createMoons, useDebug } from './use3d'
 
-const scene = ref(null) as Ref<THREE.Scene | null>
-const renderer = ref(null) as Ref<RendererPublicInterface | null>
+const scene: Ref<THREE.Scene | null> = ref(null)
+const renderer: Ref<RendererPublicInterface | null> = ref(null)
 
 const earthTilt = 23.4 * Math.PI / 180; // tilt in radians
+
+const debug = useDebug(false)
 
 function onError(err: any) {
   console.error(err)
@@ -50,23 +53,40 @@ function onReady(model: THREE.Group) {
     model.children[0].rotateOnAxis(earthAxis, 0.002)
   })
 
-  createNewMoon(5)
+  const orbit = createMoons(5, { renderer: renderer as Ref<RendererPublicInterface>, angle: earthTilt })
+  scene.value!.add(orbit)
 }
 
 function createNewMoon(nbItems: number) {
+  // const plane = new THREE.PlaneGeometry(10, 10)
+  // const material = new THREE.MeshLambertMaterial({ color: 0x7eb3e7, side: THREE.DoubleSide, opacity: 0.5, transparent: true });
+  // const referential = new THREE.Mesh(plane, material)
   const referential = new THREE.Object3D()
   for(let i = 0; i < nbItems; i++) {
-    const material = new THREE.MeshLambertMaterial({ color: 0xE0AD12, side: THREE.DoubleSide });
+    const moonMaterial = new THREE.MeshLambertMaterial({ color: 0xE0AD12, side: THREE.DoubleSide });
     const geometry = new THREE.SphereGeometry(0.3, 50, 50);
-    const moonMesh = new THREE.Mesh(geometry, material);
+    const moonMesh = new THREE.Mesh(geometry, moonMaterial);
     moonMesh.castShadow = true
     moonMesh.receiveShadow = true;
-    moonMesh.position.set(Math.cos(i * 2*Math.PI/nbItems) * 2, 0, - Math.sin(i * 2*Math.PI/nbItems) * 2);
+    moonMesh.position.set(Math.cos(i * 2*Math.PI/nbItems) * 2, - Math.sin(i * 2*Math.PI/nbItems) * 2, 0);
     referential.add(moonMesh)
   }
-  referential.rotateOnAxis(new THREE.Vector3(Math.sin(earthTilt), Math.cos(earthTilt), 0), 10)
+
+  const axis = new THREE.Vector3(Math.sin(earthTilt), 0, Math.cos(earthTilt))
+  referential.rotateOnAxis(axis, 1)
+
+  renderer.value!.onBeforeRender(() => {
+    referential.rotateOnAxis(new THREE.Vector3(0, 0, 1), 0.002)
+  })
+
   scene.value!.add(referential)
 }
+
+onMounted(() => {
+  if (debug.value) {
+    scene.value!.add(useHelperGrid());
+  }
+})
 </script>
 
 <style>
